043e36800ba499f71da81e2364c7037d11dbc20b
Kaj Heskes <162336428+DutchSapper@users.noreply.github.com>
2026-04-08 14:56:48 +0200

    Update data.sql

---
+++ sql/data.sql
@@ -0,0 +1,19 @@
+-- data.sql fills the database with demo articles.
+
+-- The right database is selected.
+USE crud_articles
+
+-- Table is created if it does not exist.
+CREATE TABLE IF NOT EXISTS articles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(5,2)
+);
+
+-- Demo-data is created
+INSERT INTO articles (name, price) VALUES
+('Milk', '1.50'),
+('Bread', '2.50'),
+('Eggs', '2.00'),
+('Cheese', '8.00'),
+('Butter','5.00');
