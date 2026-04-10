0b50d1a9fbf46834f495870c8ef153f89c77a46f
Kaj Heskes <162336428+DutchSapper@users.noreply.github.com>
2026-04-10 19:31:18 +0200

    Update

---
+++ controllers/ArticleController.php
@@ -0,0 +1,12 @@
+<?php
+    require_once __DIR__ . '/../models/Article.php';
+
+    class ArticleController {
+
+        public function index() {
+            $articles = Article::all();
+
+            require_once  __DIR__ . '/../views/article/index.php';
+        }
+    }
+?>
---
+++ models/Article.php
@@ -0,0 +1,13 @@
+<?php
+
+    class Article {
+
+        public static function all() {
+            $db = new PDO('mysql:host=localhost;dbname=crud_articles;charset=utf8', 'root', '');
+
+            $req = $db->query('SELECT * FROM articles');
+            return $req->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+    }
+?>
